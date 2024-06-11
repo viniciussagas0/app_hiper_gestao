7e1c79da4c6ead0e030c82b371bc043baa17e17d
viniciussagas0 <vinicius.s.sagas0@gmail.com>
2024-06-10 21:04:40 -0300

    Request
    using Request

---
+++ storage/framework/views/65d84d436360de6818e9dfdf2a7218315e18bee5.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - <?php echo $__env->yieldContent('titulo'); ?></title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="<?php echo e(asset('css/estilo_basico.css')); ?>" />
+    </head>
+    <body>
+    <?php echo $__env->make('site.layouts._partials.topo', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <?php echo $__env->yieldContent('conteudo'); ?>
+
+    </body>
+</html><?php /**PATH C:\Users\Vinicius\Documents\GitHub\app_hiper_gestao\resources\views/site/layouts/basico.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/87d40e96a4475c9f94f988cf691f1712cf764f85.php
@@ -0,0 +1,59 @@
+
+
+<?php $__env->startSection('titulo', 'Contato'); ?>
+
+<?php $__env->startSection('conteudo'); ?>
+<div class="topo">
+
+    <div class="logo">
+        <img src="<?php echo e(asset('img/logo.png')); ?>">
+    </div>
+
+    <div class="menu">
+        <ul>
+            <li><a href="<?php echo e(route('site.index')); ?>">Principal</a></li>
+            <li><a href="<?php echo e(route('site.sobrenos')); ?>">Sobre Nós</a></li>
+            <li><a href="<?php echo e(route('site.contato')); ?>">Contato</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="conteudo-pagina">
+    <div class="titulo-pagina">
+        <h1>Entre em contato conosco</h1>
+    </div>
+
+    <div class="informacao-pagina">
+        <div class="contato-principal">
+            <?php $__env->startComponent('site.layouts._components.form_contato', ['classe' => 'borda-preta']); ?>
+            <p>Nossa equipe retornara brevemente</p>
+            <p>respostá média de 48horas.</p>
+            <?php if (isset($__componentOriginal5fd4ee279ef26e11bb1719135c3a842ac3f4e58e)): ?>
+<?php $component = $__componentOriginal5fd4ee279ef26e11bb1719135c3a842ac3f4e58e; ?>
+<?php unset($__componentOriginal5fd4ee279ef26e11bb1719135c3a842ac3f4e58e); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+        </div>
+    </div>  
+</div>
+
+<div class="rodape">
+    <div class="redes-sociais">
+        <h2>Redes sociais</h2>
+        <img src="<?php echo e(asset('img/facebook.png')); ?>">
+        <img src="<?php echo e(asset('img/linkedin.png')); ?>">
+        <img src="<?php echo e(asset('img/youtube.png')); ?>">
+    </div>
+    <div class="area-contato">
+        <h2>Contato</h2>
+        <span>(11) 3333-4444</span>
+        <br>
+        <span>supergestao@dominio.com.br</span>
+    </div>
+    <div class="localizacao">
+        <h2>Localização</h2>
+        <img src="img/mapa.png">
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('site.layouts.basico', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Vinicius\Documents\GitHub\app_hiper_gestao\resources\views/site/contato.blade.php ENDPATH**/ ?>
