9e4f3cff73e73518b241f406f5a8dab3a30481fd
viniciussagas0 <122366041+viniciussagas0@users.noreply.github.com>
2024-06-12 16:51:12 -0300

    request new
    request to DB

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,17 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        echo '<pre>';
+        /*
+        echo '<prev>';
         print_r($request->all());
-        echo '</pre>';
+        echo '</prev>';
+        echo '<br>';
         echo $request->input('nome');
         echo '<br>';
         echo $request->input('email');
+        */
+
+        $contato = new SiteContato();
+        /*
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        */
+/*
+        $contato->fill($request->all());
+
+      //  print_r($contato->getAttributes());
+        $contato->save();
+*/
+        
         return view('site.contato');
+
+    }
+
+    public function salvar (Request $request) {
+      SiteContato::create($request->all(0));
     }
 }
