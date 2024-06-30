112a4d2e4dd83103c3cc40819bdd89856c904f6a
viniciussagas0 <vinicius.s.sagas0@gmail.com>
2024-06-30 18:23:09 -0300

    mw
    middleware learning

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::middleware(LogAcessoMiddleware::class)
+->get('/', 'PrincipalController@principal')
+->name('site.index');
+
+Route::middleware(LogAcessoMiddleware::class)
+->get('/sobre-nos', 'SobreNosController@sobreNos')
+->name('site.sobrenos');
+
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 Route::get('/login', function(){ return 'login';})->name('site.login');
